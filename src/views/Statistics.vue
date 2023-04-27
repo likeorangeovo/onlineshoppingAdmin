a9622eb4cad2f46b26e3a8b3802e46b75d039037
likeorangeovo <845156436@qq.com>
2023-04-27 23:05:26 +0800

    用户注册量统计图

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,16 +1,19 @@
 <template lang="">
   <div>
     <e-charts class="chart" :option="option" />
+    <e-charts class="chart" :option="userOption" />
   </div>
 </template>
 <script>
 import { ref, computed, onBeforeMount } from 'vue'
-import { getOrder } from '../request/index.js'
+import { getOrder, getUserInfo } from '../request/index.js'
 export default {
   setup() {
     let dates = ref([])
     let sales = ref([])
     let option = ref({})
+    let userOption = ref({})
+    let userInfoDatas = ref([])
     onBeforeMount(async () => {
       const res = await getOrder()
       const orders = [...res.data.data]
@@ -22,7 +25,6 @@ export default {
       }, {});
       // 将销售数据按日期排序
       const sortedSales = Object.entries(salesByDay).sort((a, b) => a[0].localeCompare(b[0]));
-      console.log(sortedSales)
       dates.value = sortedSales.map(sale => sale[0]);
       sales.value = sortedSales.map(sale => sale[1]);
       option.value = {
@@ -45,12 +47,49 @@ export default {
           data: sales.value
         }]
       }
+
+      //用户注册统计图
+      const userInfoRes = await getUserInfo()
+      const users = [...userInfoRes.data.data]
+      users.forEach(user => {
+        const date = user.create_time.slice(0, 10);
+        const item = userInfoDatas.value.find(item => item.date === date);
+        if (item) {
+          item.count++;
+        } else {
+          userInfoDatas.value.push({ date, count: 1 });
+        }
+      })
+      userOption.value = {
+        title: {
+          text: '用户注册量统计图'
+        },
+        tooltip: {},
+        legend: {
+          data: ['用户注册量']
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: userInfoDatas.value.map(item => item.date)
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          name: '用户注册量',
+          type: 'line',
+          data: userInfoDatas.value.map(item => item.count)
+        }]
+      };
     })
 
     return {
       dates,
       sales,
-      option
+      option,
+      userOption,
+      userInfoDatas
     }
   }
 }
